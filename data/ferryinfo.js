2abe7bfc0fbf3f9d864eddbfe0ee274c32bf0434
Kyösti Ranto <kranto@gmail.com>
2018-01-13 22:55:57 +0200

    infotest updates, new infotest.css and ferryinfo.js

---
+++ data/ferryinfo.js
@@ -0,0 +1,892 @@
+ferries = {
+	skiftet: {
+		name: "M/S Skiftet",
+		features: {
+			access: true,
+			cafe: true
+		},
+		capacity: {
+			persons: 200,
+			cars: 20,
+			trucks: 4
+		},
+		contact: {
+			phones: [ '+358 (0)400 350 265', {name: 'Café', tel: '+358 (0)405 515 645'}]
+		},
+	},
+	gudingen: {
+		name: "M/S Gudingen",
+		features: {
+			access: true,
+			cafe: true,
+		},
+		capacity: {
+			persons: 195,
+			cars: 20,
+			trucks: 4
+		},
+		contact: {
+			phones: [ '+358 (0)40 769 3687', {name: 'Café', tel: '+358 (0)405 505 059'}]
+		},
+	},
+	ejdern: {
+		name: "M/S Ejdern",
+		features: {
+			access: false,
+			cafe: false,
+		},
+		capacity: {
+			persons: 144,
+			cars: 16,
+			trucks: 4,
+		},
+		contact: {
+			phones: ['+358 (0)40 0229 260']
+		},
+	},
+	skarven: {
+		name: "M/S Skarven",
+		features: {
+			access: true,
+			cafe: true,
+		},
+		capacity: {
+			persons: 250,
+			cars: 60,
+			trucks: 4,
+		},
+		contact: {
+			phones: ['+358 (0)40 173 3600',{name: 'Café', tel: '+358 (0)457 3439 301'}]
+		},
+	},
+	alfageln: {
+		name: "M/S Alfågeln",
+		features: {
+			access: true,
+			cafe: true,
+		},
+		capacity: {
+			persons: 244,
+			cars: 50,
+			trucks: 4,
+		},
+		contact: {
+			phones: ['+358 (0)40 768 2108',{name: 'Café', tel: '+358 (0)40 637 8500'}]
+		},
+	},
+	knipan: {
+		name: "M/S Knipan",
+		features: {
+			access: true,
+			cafe: false,
+		},
+		capacity: {
+			persons: 157,
+			cars: 20,
+			trucks: 4,
+		},
+		contact: {
+			phones: ['+358 (0)40 0229 261']
+		},
+	},
+	viggen: {
+		name: "M/S Viggen",
+		features: {
+			access: true,
+			cafe: true,
+		},
+		capacity: {
+			persons: 250,
+			cars: 50,
+			trucks: 4,
+		},
+		contact: {
+			phones: ['+358 (0)40 0330 455',{name: 'Café', tel: '+358 (0)40 7658 453'}]
+		},
+	},
+	doppingen: {
+		name: "M/S Doppingen",
+		features: {
+			access: false,
+			cafe: false,
+		},
+		capacity: {
+			persons: 70,
+			cars: 18,
+			trucks: 2,
+		},
+		contact: {
+			phones: ['+358 (0)40 7082 136']
+		},
+	},
+	rosala: {
+		name: "M/S Rosala II",
+		features: {
+			access: false,
+			cafe: false,
+		},
+		capacity: {
+			persons: 65,
+			cars: 6,
+			trucks: 1,
+		},
+		contact: {
+			phones: ['+358 (0)40 0492 739']
+		},
+	},
+	frida: {
+		name: "M/S Frida",
+		features: {
+			access: false,
+			cafe: false,
+		},
+		capacity: {
+			persons: 17,
+			cars: 18,
+			trucks: 2,
+		},
+		contact: {
+			phones: ['+358 (0)457 3613 394']
+		},
+	},
+	odin: {
+		name: "M/S Odin",
+		features: {
+			access: true,
+			cafe: false,
+		},
+		capacity: {
+			persons: 80,
+			cars: 45,
+			trucks: 3,
+		},
+		contact: {
+			phones: ['+358 (0)40 4840 352']
+		},
+	},
+	hammaronsalmi: {
+		name: "Lautta #185",
+		features: {
+		},
+		capacity: {
+			cars: 14,
+		},
+		contact: {
+			phones: ['+358 40 558 2773']
+		},
+	},
+	hogsar: {
+		name: "Lautta #182",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 40 558 4412']
+		},
+	},
+	hogsara: {
+		name: "Lautta #151",
+		features: {
+		},
+		capacity: {
+			cars: 14,
+		},
+		contact: {
+			phones: ['+358 40 706 6291']
+		},
+	},
+	aura: {
+		name: "M/S Aura",
+		features: {
+		},
+		capacity: {
+			cars: 52,
+			persons: 197,
+		},
+		contact: {
+			phones: ['+358 400 320 093']
+		},
+	},
+	aurora: {
+		name: "M/S Aurora",
+		features: {
+		},
+		capacity: {
+			cars: 52,
+			persons: 197,
+		},
+		contact: {
+			phones: ['+358 400 320 092']
+		},
+	},
+	keistio: {
+		name: "Lautta #177",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 40 846 9500']
+		},
+	},
+	kivimolossi: {
+		name: "Lautta #167",
+		features: {
+		},
+		capacity: {
+			cars: 14,
+		},
+		contact: {
+			phones: ['+358 400 292 691']
+		},
+	},
+	kokkila: {
+		name: "Lautta #199",
+		features: {
+		},
+		capacity: {
+			cars: 21,
+		},
+		contact: {
+			phones: ['+358 40 568 4911']
+		},
+	},
+	stella_korppoo: {
+		name: "M/S Stella",
+		features: {
+		},
+		capacity: {
+			cars: 65,
+		},
+		contact: {
+			phones: ['+358 400 114 291']
+		},
+	},
+	mergus: {
+		name: "M/S Mergus",
+		features: {
+		},
+		capacity: {
+			cars: 27,
+		},
+		contact: {
+			phones: ['+358 400 533 461']
+		},
+	},
+	mossala: {
+		name: "Lautta #176",
+		features: {
+		},
+		capacity: {
+			cars: 14,
+		},
+		contact: {
+			phones: ['+358 40 553 1534']
+		},
+	},
+	prostvik1: {
+		name: "M/S Prostvik 1",
+		features: {
+		},
+		capacity: {
+			cars: 30,
+		},
+		contact: {
+			phones: ['+358 400 864 268']
+		},
+	},
+	nagu2: {
+		name: "M/S Nagu 2",
+		features: {
+		},
+		capacity: {
+			cars: 16,
+		},
+		contact: {
+			phones: ['+358 400 882 614']
+		},
+	},
+	falko: {
+		name: "M/S Falkö",
+		features: {
+		},
+		capacity: {
+			cars: 8,
+			persons: 110,
+		},
+		contact: {
+			phones: ['+358 400 320 097']
+		},
+	},
+	palva: {
+		name: "Lautta #179",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 400 525 901']
+		},
+	},
+	elektra: {
+		name: "M/S Elektra",
+		features: {
+		},
+		capacity: {
+			cars: 90,
+			persons: 450,
+		},
+		contact: {
+			phones: ['+358 40 662 1320']
+		},
+	},
+	sterna: {
+		name: "M/S Sterna",
+		features: {
+		},
+		capacity: {
+			cars: 66,
+		},
+		contact: {
+			phones: ['+358 400 127 999']
+		},
+	},
+	falco: {
+		name: "M/S Falco",
+		features: {
+		},
+		capacity: {
+			cars: 54,
+		},
+		contact: {
+			phones: ['+358 400 110 114']
+		},
+	},
+	viken: {
+		name: "M/S Viken",
+		features: {
+		},
+		capacity: {
+			cars: 14,
+			persons: 73,
+		},
+		contact: {
+			phones: ['+358 400 320 099']
+		},
+	},
+	antonia: {
+		name: "M/S Antonia",
+		features: {
+		},
+		capacity: {
+			cars: 27,
+		},
+		contact: {
+			phones: ['+358 400 320 049']
+		},
+	},
+	saverkeit: {
+		name: "Lautta #181",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 40 553 1072']
+		},
+	},
+	vartsala: {
+		name: "Lautta #198",
+		features: {
+		},
+		capacity: {
+			cars: 36,
+		},
+		contact: {
+			phones: ['+358 400 138 239']
+		},
+	},
+	velkuanmaa: {
+		name: "Lautta #171",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 400 525 902']
+		},
+	},
+	mskivimo: {
+		name: "M/S Kivimo",
+		features: {
+		},
+		capacity: {
+			cars: 8,
+			persons: 50,
+		},
+		contact: {
+			phones: ['+358 400 320 095']
+		},
+	},
+	vano: {
+		name: "Lautta #201",
+		features: {
+		},
+		capacity: {
+			cars: 21,
+		},
+		contact: {
+			phones: ['+358 500 827 157']
+		},
+	},
+	skagen: {
+		name: "Vikare",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 10 271 7915']
+		},
+	},
+	sorpo: {
+		name: "Sorpo",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 40 569 2620']
+		},
+	},
+	pettu: {
+		name: "Pettu",
+		features: {
+		},
+		capacity: {
+			cars: 10,
+		},
+		contact: {
+			phones: ['+358 50 354 2488']
+		},
+	},
+	satava: {
+		name: "M/S Satava",
+		features: {
+		},
+		capacity: {
+			cars: 8,
+			persons: 80,
+		},
+		contact: {
+			phones: ['+358 40 849 5140']
+		},
+	},
+	nordep: {
+		name: "M/S Nordep",
+		features: {
+		},
+		capacity: {
+			cars: 2,
+			persons: 78,
+		},
+		contact: {
+			phones: ['+358 45 129 4779']
+		},
+	},
+	karolina: {
+		name: "M/S Karolina",
+		features: {
+		},
+		capacity: {
+			persons: 30,
+		},
+		contact: {
+			phones: ['+358 40 717 3455 ']
+		},
+	},
+	cheri: {
+		name: "M/S Cheri",
+		features: {
+		},
+		capacity: {
+			persons: 12,
+		},
+		contact: {
+			phones: ['+358 400 217 053', '+358 400 227 722']
+		},
+	},
+	myrskylintu: {
+		name: "M/S Myrskylintu",
+		features: {
+		},
+		capacity: {
+			persons: 28,
+		},
+		contact: {
+			phones: ['+358 400 217 053', '+358 400 227 722']
+		},
+	},
+	fisko: {
+		name: "Y/A Fiskö",
+		features: {
+		},
+		capacity: {
+			cars: 2,
+			persons: 36,
+		},
+		contact: {
+			phones: ['+358 44 761 8000'],
+			email: 'info@saaristolinjat.fi',
+			email_sv: 'info@skargardslinjer.fi'
+		},
+	},
+	isla: {
+		name: "M/S Isla",
+		features: {
+		},
+		capacity: {
+			cars: 8,
+			persons: 100,
+		},
+		contact: {
+			phones: ['+358 40 673 6697'],
+			fb: 'https://www.facebook.com/MS-Isla-401567819993437/'
+		},
+	},
+	stella_hiittinen: {
+		name: "M/S Stella",
+		features: {
+		},
+		capacity: {
+			cars: 8,
+			persons: 100,
+		},
+		contact: {
+			phones: ['+358 40 675 6441'],
+			fb: 'https://www.facebook.com/MS-Stella-1408172536122848/'
+		},
+	},
+	alva: {
+		name: "M/S Alva",
+		features: {
+		},
+		capacity: {
+			persons: 20,
+		},
+		contact: {
+			phones: ['+358 40 413 1574']
+		},
+	},
+	eivor: {
+		name: "M/S Eivor",
+		features: {
+			cafe: true
+		},
+		capacity: {
+			cars: 19,
+			persons: 195
+		},
+		contact: {
+			phones: ['+358 44 5000 503', {name: 'Café', tel: '+358 44 5000 506'}],
+			email: 'eivor@rosita.fi',
+			fb: 'https://www.facebook.com/mseivor/'
+		}
+	},
+	silvana: {
+		name: "M/S Silvana",
+		capacity: {
+			bikes: 50,
+			persons: 50
+		},
+		contact: {
+			phones: ['+358 400 229149']
+		}
+	}
+}
+
+operators = {
+	alandstrafiken: {
+		name: "Ålandstrafiken",
+		logo: "alandstrafiken.png",
+		contact: {
+			phones: ['+358-18-25 600'],
+			fax: '+358-18-17815',
+			address: 'Styrmansgatan 1, AX-22100 MARIEHAMN',
+			fb: 'https://www.facebook.com/%C3%85landstrafiken-trafikinformation-159908027409016/',
+			email: 'info@alanstrafiken.ax',
+			www: 'http://www.alandstrafiken.ax',
+			www_fi: 'http://www.alandstrafiken.ax/fi',
+			www_sv: 'http://www.alandstrafiken.ax/sv',
+			www_en: 'http://www.alandstrafiken.ax/en',
+		},
+	},
+	finferries: {
+		name: "Finferries",
+		logo: "finferries.png",
+		contact: {
+			phones: ['+358 207 118 750', {name: "Timetables", name_fi: "Aikatauluneuvonta", name_sv: "Tidtabellsinformation", tel: '+358 400 117 123'}],
+			address: 'Puutarhakatu 55-57, PL 252, 20101 Turku',
+			www: 'http://www.finferries.fi/',
+			www_fi: 'http://www.finferries.fi/',
+			www_sv: 'http://www.finferries.fi/sv',
+			www_ev: 'http://www.finferries.fi/en'
+		},
+	},
+	ncl: {
+		name: "Nordic Coast Line / Nordic Jetline",
+		logo: "ncl.png",
+		contact: {
+			address: 'Mannerheiminkatu 20, 06100 Porvoo',
+			phones: ['+358 19 536 2200'],
+			fax: '+358 19 536 2201',
+			email: 'info@nordiccoastline.fi',
+			www: 'http://www.ncl.fi/fi',
+			www_fi: 'http://www.ncl.fi/fi',
+			www_sv: 'http://www.ncl.fi/sv',
+			www_en: 'http://www.ncl.fi/en'
+		},
+	},
+	ferryway: {
+		name: "JS Ferryway Ltd Oy",
+		logo: "ferryway.png",
+		contact: {
+			phones: [],
+			email: 'info@ferryway.fi',
+			address: 'Äpplö, 21760 Houtskari',
+			www: 'https://www.ferryway.fi/'
+		},
+	},
+	al: {
+		name: "Archipelago Lines",
+		logo: "archipelagolines.png",
+		contact: {
+			phones: ['+358 41 456 4828', '+358 45 124 5551'],
+			email: 'info@a-lines.fi',
+			www: 'http://www.saaristolinjat.fi/',
+			www_fi: 'http://saaristolinjat.fi/',
+			www_sv: 'http://skargardslinjer.fi/'
+		},
+	},
+	savolainen: {
+		name: "Kuljetus Savolainen Oy",
+		logo: 'kuljetussavolainen.png',
+		contact: {
+			phones: ['+358 6 533 0542', '+358 400 849444'],
+			fax: '+358 6 5350500',
+			email: 'info@kuljetus-savolainen.fi',
+			www: 'https://kuljetus-savolainen.fi/yhteysalusliikenne/'
+		},
+	},
+	rosita: {
+		name: "Rosita Oy",
+		logo: "rosita.png",
+		contact: {
+			phones: ['+358 2 213 1500'],
+			email: 'info@rosita.fi',
+			address: 'Formaalintie 10, 20780 Kaarina',
+			www: 'http://www.rosita.fi/',
+			www_fi: 'http://www.rosita.fi/',
+			www_sv: 'http://www.rosita.fi/Hemsida'
+		},
+	}
+}
+
+piers = {
+	svino: {
+		name: "Svinö",
+		mun: "Lumparland",
+		type: "1",
+		distances: [
+			{
+				to: "Mariehamn",
+				to_fi: "Maarianhamina",
+				dist: 27
+			},{
+				to: "Långnäs",
+				dist: 7.1
+			}
+		]
+	},
+	degerby: {
+		name: "Degerby",
+		mun: "Föglö",
+		type: "1",
+		distances: [
+			{
+				to: "Överö",
+				dist: 17
+			}
+		]
+	}
+}
+
+timetables = {
+	foglolinjen: [
+		{ validFrom: "2018-01-01", validTo: "2018-05-31", tables: ["FoglolinjenVinter_1.png", "FoglolinjenVinter_2.png"]},
+		{ validFrom: "2018-06-01", validTo: "2018-08-31", tables: ["FoglolinjenSommar_1.png", "FoglolinjenSommar_2.png"]},
+		{ validFrom: "2018-09-01", validTo: "2018-12-31", tables: ["FoglolinjenVinter_1.png", "FoglolinjenVinter_2.png"]}
+	]
+}
+
+pricelists = {
+	alandstrafiken: [
+		{ validFrom: "2018-01-01", validTo: "2018-12-31", lists: ["AlandstrafikenPricelist2018_1.png", "AlandstrafikenPricelist2018_2.png"]}
+	]
+}
+
+routes = {
+	foglolinjen: {
+		name: "Föglölinjen",
+		name_fi: "Föglön linja",
+		name_en: "Föglo line",
+		specifier: "Svinö - Degerby",
+		piers: ["ref_piers_svino", "ref_piers_degerby"],
+		operator: "ref_operators_alandstrafiken",
+		vessels: ["ref_ferries_skarven"],
+		features: {
+			interval: "8-12 daily",
+			interval_fi: "8-12 vuoroa päivässä",
+			interval_sv: "8-12 gånger om dagen",
+			booking: false,
+			cost: true,
+			duration: "35 minutes",
+			duration_fi: "35 minuuttia",
+			duration_sv: "35 minuter"
+		},
+		timetables: "ref_timetables_foglolinjen",
+		pricelists: "ref_pricelists_alandstrafiken"
+	}
+}
+
+ferrydata = {
+	routes: routes,
+	ferries: ferries,
+	operators: operators,
+	piers: piers,
+	timetables: timetables,
+	pricelists: pricelists,
+}
+
+function sanitizePhone(num) {
+	return num.replace(/\(.*\)/g, "").replace(/[-]/g, " ");
+}
+
+function phoneUri(num) {
+	return num.replace(/\(.*\)/g, "").replace(/[ -]/g, "");
+}
+
+function getPhones(item) {
+	if (item.phones) {
+		var phones = Array.isArray(item.phones)? item.phones: [item.phones];
+		return phones.map(function(phone) {
+			if (typeof phone === 'object') {
+				return { name: phone.name, tel: sanitizePhone(phone.tel), uri: phoneUri(phone.tel) };
+			} else {
+				return { name: "", tel: sanitizePhone(phone), uri: phoneUri(phone) };
+			}
+		});
+	} else {
+		return [];
+	}
+}
+
+function getLangProperty(item, property, lang) {
+	var result = null;
+	var localizedProperty = property + "_" + lang; 
+	for (let key of Object.keys(item)) {
+		if (key === property) result = item[key];
+		if (key === localizedProperty) return item[key];
+	}
+	return result;
+}
+
+function getLocalizedItem(item, lang) {
+	
+	if (!(item instanceof Object)) {
+		if (typeof item === 'string' && item.startsWith("ref_")) {
+			var parts = item.split("_");
+			var sub = ferrydata[parts[1]][parts[2]];
+			return getLocalizedItem(deepCopy(sub));
+		} else {
+			return item;
+		}
+	}
+	if (item instanceof Array) {
+		return item.map(function(i) { return getLocalizedItem(i, lang); });
+	}
+
+	var result = {}
+	for (let key of Object.keys(item)) {
+		if (key.endsWith("_" + lang)) {
+			var key1 = key.substring(0, key.length - 3);
+			result[key1] = item[key];
+		} else if (!/_..$/.test(key) && !result[key]) {
+			result[key] = getLocalizedItem(item[key], lang);
+		}
+	}
+	return result;
+}
+
+function getWww(item, lang) {
+	return getLangProperty(item, 'www', lang);
+}
+
+function getEmail(item, lang) {
+	return getLangProperty(item, 'email', lang);
+}
+
+function deepCopy(object) {
+	return JSON.parse(JSON.stringify(object));
+}
+
+function routeInfo(route, lang) {
+	route = getLocalizedItem(deepCopy(route), lang);
+	var info = {};
+	var contacts = [];
+	info.name = route.name;
+	info.specifier = route.specifier? route.specifier: "";
+	info.vessels = route.vessels;
+	info.vessels.forEach(function(vessel) {
+		vessel.contact.name = vessel.name;
+		contacts.push(vessel.contact);
+	});
+	info.piers = route.piers;
+
+	info.operator = route.operator;
+	info.operator.contact.name = info.operator.name;
+	contacts.push(info.operator.contact);
+	info.features = route.features;
+
+	info.timetables = route.timetables;
+	info.pricelists = route.pricelists;
+
+	info.contacts = contacts;
+
+
+	return info;
+}
+
+console.log(getPhones(operators.savolainen.contact));
+console.log(getPhones(ferries.gudingen.contact));
+console.log(getPhones(operators.ferryway.contact));
+
+
+console.log(routes.foglolinjen);
+
+console.log(piers.degerby);
+console.log(getLocalizedItem(operators.alandstrafiken, "fi"));
+console.log(getLocalizedItem(operators.alandstrafiken, "sv"));
+console.log(getLocalizedItem(operators.alandstrafiken, "en"));
+
+route = routeInfo(routes.foglolinjen, "fi");
+console.log(route);
+console.log(route.contacts[1].phones)
