190ba9c35d5a9ca5d74efec08606e1964742e223
Kyösti Ranto <kranto@gmail.com>
2018-06-05 23:13:28 +0300

    updated version

--- javascripts/partialloader.js
+++ javascripts/partialloader.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	$("#infotemplateholder").load("templates/infocontent.html?v=1 #infocontenttemplate");
 	$("#infotemplate2holder").load("templates/infocontent2.html?v=1.1 #infocontent2template");
-	$("#timetabletemplateholder").load("templates/timetabledialog.html?v=1 #timetabletemplate");
+	$("#timetabletemplateholder").load("templates/timetabledialog.html?v=1.1 #timetabletemplate");
 	$("#contactstemplateholder").load("templates/contactsdialog.html?v=1 #contactstemplate");
 
 });
