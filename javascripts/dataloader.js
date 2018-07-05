12f6bf3c22f41aa325c042de4dc9709e06015fd6
Kyösti Ranto <kranto@gmail.com>
2018-07-05 22:00:35 +0300

    knipan to södra linjen, ejdern to norra

--- javascripts/dataloader.js
+++ javascripts/dataloader.js
@@ -23,7 +23,7 @@ $.get('/data/routes.json?v=1', function(data) {
 
 var fdata;
 
-$.get('/data/data.json?v=1.14', function(data) {
+$.get('/data/data.json?v=1.15', function(data) {
   prepareData(data);
   fdata = data;
   sendDataIf();
