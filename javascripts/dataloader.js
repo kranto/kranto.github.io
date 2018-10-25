d101ab43455b481c7fdeec08bbac746dc6a6c50d
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-10-25 22:15:16 +0300

    updated versions

--- javascripts/dataloader.js
+++ javascripts/dataloader.js
@@ -23,7 +23,7 @@ $.get('/data/routes.json?v=1', function(data) {
 
 var fdata;
 
-$.get('/data/data.json?v=1.24', function(data) {
+$.get('/data/data.json?v=1.25', function(data) {
   prepareData(data);
   fdata = data;
   sendDataIf();
